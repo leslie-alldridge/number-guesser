ea8d4b39b763a2d3718f40050ebe9e73f4fd077b
Leslie Alldridge <leslie.alldridge@gmail.com>
2018-11-03 18:54:05 +1300

    init
    init

---
+++ NumberGuesser/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace NumberGuesser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //app setup
+            string appName = "Number Guesser";
+            string appVersion = "1.0.0";
+            string appAuthor = "Leslie Alldridge";
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("{0}: Version {1} by {2}", appName, appVersion, appAuthor);
+            Console.ResetColor();
+            //change text color
+        }
+    }
+}
