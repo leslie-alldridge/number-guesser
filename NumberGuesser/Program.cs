0c444c623861c79cd887d392053da7305e889987
Leslie Alldridge <leslie.alldridge@gmail.com>
2018-11-03 18:56:23 +1300

    grabs user name
    grabs user name

--- NumberGuesser/Program.cs
+++ NumberGuesser/Program.cs
@@ -11,10 +11,19 @@ namespace NumberGuesser
             string appVersion = "1.0.0";
             string appAuthor = "Leslie Alldridge";
 
+            //change console text color and write out app info
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("{0}: Version {1} by {2}", appName, appVersion, appAuthor);
             Console.ResetColor();
-            //change text color
+
+            //Ask users name
+            Console.WriteLine("What is your name?");
+
+            //Get name and save it to a string
+            string input = Console.ReadLine();
+
+            //Write out the greeting
+            Console.WriteLine("Hello {0}, let's play a game...", input);
         }
     }
 }
