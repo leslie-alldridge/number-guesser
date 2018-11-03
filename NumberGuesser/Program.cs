3f2b88b64ceb4e321c65302c5c9e5b858db42add
Leslie Alldridge <leslie.alldridge@gmail.com>
2018-11-03 19:02:53 +1300

    number guessing logic works
    number guessing logic works

--- NumberGuesser/Program.cs
+++ NumberGuesser/Program.cs
@@ -20,10 +20,38 @@ namespace NumberGuesser
             Console.WriteLine("What is your name?");
 
             //Get name and save it to a string
-            string input = Console.ReadLine();
+            string inputName = Console.ReadLine();
 
             //Write out the greeting
-            Console.WriteLine("Hello {0}, let's play a game...", input);
+            Console.WriteLine("Hello {0}, let's play a game...", inputName);
+
+            //Init with correct number
+            int correctNumber = 7;
+
+            //Guess var
+            int guess = 0;
+
+            Console.WriteLine("Guess a number between 1 and 10");
+
+            //while guess is not correct
+            while(guess != correctNumber)
+            {
+                //get input
+                string input = Console.ReadLine();
+                //parse input to int
+                guess = Int32.Parse(input);
+                //match guess to correct number
+                if(guess != correctNumber)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Wrong number. Please try again.");
+                    Console.ResetColor();
+                }
+            }
+            //output success message 
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Correct!!");
+            Console.ResetColor();
         }
     }
 }
